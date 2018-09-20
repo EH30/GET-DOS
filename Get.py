0920bf18f43dd10f881c4fef82be74b75ad18718
EH30 <games.eh2000@gmail.com>
2018-09-20 15:02:11 +0530

    Initial commit

---
+++ Get.py
@@ -0,0 +1,38 @@
+import os
+import sys
+import random
+
+##################################
+# Educational purpose only #
+################################# #
+# I'm not responsible for your actions #
+######################################################################
+# SUBSCRIBE: https://www.youtube.com/channel/UCKAmv8p_TRvUNrJlfiB8qBQ#
+######################################################################
+
+
+SUB = "SUB"
+kali = "kali"
+SUBSCRIBE = "To My YouTube Channel"
+
+
+print "-----------------------------------------------------------------------"
+print "                  Check README.txt for Username and Password           "
+print "SUBSCRIBE: https://www.youtube.com/channel/UCKAmv8p_TRvUNrJlfiB8qBQ    "
+print "-----------------------------------------------------------------------"
+print "                         Made by TheTechHacker                         "
+print "-----------------------------------------------------------------------"
+
+user = input("Username: ")
+passwd = input("Password: ")
+
+if user is "SUB":
+    if passwd is "kali":
+        print "Hello Admin"
+        os.system("sudo apt-get install gcc")
+        os.system("sudo apt-get install figlet")
+        os.system("sudo apt-get install git")
+        os.system("figlet SUBSRIBE")
+        os.system("git clone https://github.com/EH30/DOS-Scripts")
+
+print ("SUBSCRIBE: https://www.youtube.com/channel/UCKAmv8p_TRvUNrJlfiB8qBQ ")
---
+++ README.txt
@@ -0,0 +1,7 @@
+ SUBSCRIBE: https://www.youtube.com/channel/UCKAmv8p_TRvUNrJlfiB8qBQ
+-----------------------------------------------------------------------------------
+Username: SUB
+Password: kali
+-----------------------------------------------------------------------------------
+runs on Python 2.7.15 
+
